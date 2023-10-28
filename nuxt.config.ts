17aac0d0e65d5ca5bea9bb7ad82a729d49c81b93
beejsbj <burooj.bj@gmail.com>
2023-10-27 23:22:11 -0400

    Init project

---
+++ nuxt.config.ts
@@ -0,0 +1,17 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devServer: {
+    port: 3005,
+  },
+
+  devtools: { enabled: false },
+  css: ["@/assets/styles/site.css"],
+  modules: ["@nuxt/content"],
+
+  vue: {
+    compilerOptions: {
+      //every tag with '-' is a custom element
+      isCustomElement: (tag) => tag.includes("-"),
+    },
+  },
+});
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <Header />
+  <main>
+    <HeroLanding />
+    <AboutSection />
+    <SpeakersGrid />
+  </main>
+  <Footer />
+</template>
