24c9ec88ce49d4ac1e1c44d517de5dcc74e77363
beejsbj <burooj.bj@gmail.com>
2023-10-30 18:12:07 -0400

    remove refers color dark

---
+++ components/ToggleLight.vue
@@ -0,0 +1,18 @@
+<script setup>
+const isLightOn = ref(false);
+
+function toggle() {
+  isLightOn.value = !isLightOn.value;
+
+  // set the prefer color scheme
+  const scheme = isLightOn.value ? "light" : "dark";
+
+  document.documentElement.setAttribute("data-theme", scheme);
+}
+</script>
+
+<template>
+  <button class="button" @click="toggle">Toggle</button>
+</template>
+
+<style scoped lang="scss"></style>
