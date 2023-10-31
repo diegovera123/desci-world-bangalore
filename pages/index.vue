0cf52a21e5366326e00886eb75c60cb73cb9a9ee
beejsbj <burooj.bj@gmail.com>
2023-10-31 01:21:37 -0400

    New components

--- pages/index.vue
+++ pages/index.vue
@@ -2,10 +2,19 @@
   <Header />
   <main>
     <HeroLanding />
+    <MotivateSection
+      text="Decentralised Science showcases in Bengaluru"
+      image="skyline"
+    />
+
     <AboutSection />
-    <MotivateSection />
+    <MotivateSection
+      text="WHERE SCIENCE MEETS THE FUTURE."
+      image="bangalore-motivate"
+    />
     <EventSection />
     <SpeakersGrid />
+    <RegisterNow />
   </main>
   <Footer />
 </template>
