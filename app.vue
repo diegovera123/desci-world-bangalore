817b814274e234891c25f2325ab8b130bda3f790
beejsbj <burooj.bj@gmail.com>
2023-10-31 00:56:58 -0400

    Begin working on meta

--- app.vue
+++ app.vue
@@ -1,3 +1,13 @@
+<script setup>
+useSeoMeta({
+  title: "World of DeSci: Bengaluru",
+  description:
+    "DeSci World is a conference for data scientists and machine learning engineers.",
+  //   image: "https://desci.world/images/og-image.png",
+  //   url: "https://desci.world",
+});
+</script>
+
 <template>
   <NuxtPage />
 </template>
